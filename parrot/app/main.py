4d1a3d8b2c504af9c110f3d90fd83005f248d8c3
Flynn <wphestiraid@gmail.com>
2025-04-01 09:20:10 +0900

    프로젝트 초기 설정 및 FastAPI 애플리케이션 생성
    - FastAPI 애플리케이션을 위한 초기 설정 파일 추가
    - 기본 환경 설정 및 의존성 관리 파일 추가

---
+++ parrot/app/main.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+
+from parrot.app.settings import get_settings
+
+
+def create_app() -> FastAPI:
+    settings = get_settings()
+    app = FastAPI(
+        **settings.fastapi_kwargs,
+    )
+
+    return app
+
+
+app = create_app()
